11f640327cbb37a8b4cb0313a23d00abcf22ae49
Bernardo Camarate <b.m.m.c@hotmail.com>
2024-12-22 21:03:37 +0000

    Backend changes

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,36 @@
-import express from 'express';
-import routes from './routes';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+import routes from "./routes";
 
 const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api', routes);
+
+app.get("/api/healthcheck", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/api/webhook", (req, res) => {
+  console.log(
+    `New alert received at, ${new Date().toISOString()}:\n`,
+    req.body
+  );
+  const { ticker, price, alert_name, trigger_time } = req.body;
+  io.emit("new_alert", { ticker, price, alert_name, trigger_time });
+
+  res.status(200).json({ message: "Webhook received successfully" });
+});
+io.on("connection", (socket) => {
+  console.log("Client connected to socket:", socket.id);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -6,14 +6,14 @@ router.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-router.post("/webhook", (req, res) => {
-  console.log(
-    `New alert received at, ${new Date().toISOString()}:\n`,
-    req.body
-  );
-  const { ticker, price, alert_name, trigger_time } = req.body;
+// router.post("/webhook", (req, res) => {
+//   console.log(
+//     `New alert received at, ${new Date().toISOString()}:\n`,
+//     req.body
+//   );
+//   const { ticker, price, alert_name, trigger_time } = req.body;
 
-  res.status(200).json({ message: "Webhook received successfully" });
-});
+//   res.status(200).json({ message: "Webhook received successfully" });
+// });
 
 export default router;
