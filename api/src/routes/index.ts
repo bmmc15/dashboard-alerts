6e5aece880ad4522ce04bdcb0d4d65bc1f696c7a
Bernardo Camarate <b.m.m.c@hotmail.com>
2024-12-22 20:51:06 +0000

    Webhook basic config

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,16 +1,19 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+router.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+router.post("/webhook", (req, res) => {
+  console.log(
+    `New alert received at, ${new Date().toISOString()}:\n`,
+    req.body
+  );
+  const { ticker, price, alert_name, trigger_time } = req.body;
 
-router.post('/webhook', (req, res) => {
-    console.log('Novo alerta recebido:', req.body);
-
-    res.status(200).send('Webhook recebido com sucesso');
-})
+  res.status(200).json({ message: "Webhook received successfully" });
+});
 
 export default router;
