7334904bcf3265b06766883f0b3990475d0a999e
Bernardo Camarate <b.m.m.c@hotmail.com>
2025-01-02 11:08:32 +0000

    Front-end changes

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import Alerts from './components/Alerts.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Alerts/>
   </StrictMode>,
 )
