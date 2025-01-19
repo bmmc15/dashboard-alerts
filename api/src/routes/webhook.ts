a64b28b983cc766d9e5f168f2b3991f7f3be4d4f
Bernardo Camarate <b.m.m.c@hotmail.com>
2025-01-19 11:31:40 +0000

    Testing if alertName is included in some header

--- api/src/routes/webhook.ts
+++ api/src/routes/webhook.ts
@@ -1,36 +1,42 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { Server as SocketIOServer } from "socket.io";
-import { IndicatorParser } from "../parsers/IndicatorParser";
-import { parsers } from "../parsers";
 
 const webhookRoutes = (io: SocketIOServer): Router => {
   const router = Router();
 
-  router.post("/", (req, res) => {
-    const { alert_name } = req.body;
+  router.post("/", (req: Request, res: Response) => {
+    console.log("Received Alert:", req.body);
 
-    console.log("Received Alert: ", req.body);
+    const alert: { message?: string } = {};
+    let payload;
 
-    // const parser = parsers["X48"];
+    if (req.is("text/*")) {
+      console.log("TEXT Headers:", req.headers);
+      console.log("TEXT Body:", req.body);
+      console.log("Text Alert received:", req.body);
+      alert.message = req.body;
 
-    // if (parser) {
-    //   const parsedAlert = parser.parse(req.body);
-    //   io.emit("alert", parsedAlert);
-    //   res.status(200).json({ message: "Webhook processed successfully" });
-    // } else {
-    //   res.status(400).json({ message: `No parser found for indicator:`}); // ${indicator}` });
-    // }
+      payload = {
+        type: "text-alert",
+        content: alert.message,
+        timestamp: new Date().toISOString(),
+      };
+    } else {
+      console.log("JSON Headers:", req.headers);
+      console.log("JSON Body:", req.body);
+      payload = {
+        type: "json-alert",
+        content: req.body,
+        timestamp: new Date().toISOString(),
+      };
+    }
 
-    io.emit("alert", {
-      // alertName: alert_name,
-      // timestamp: new Date().toISOString(),
-      ...req.body,
-    });
+    io.emit("alert", payload);
 
     res.status(200).json({ message: "Webhook processed successfully" });
   });
 
   return router;
-}
+};
 
 export default webhookRoutes;
